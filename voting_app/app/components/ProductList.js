a501231258402513bff61cf397150f45c10e9003
dhaval <dhaval.b.nagar@gmail.com>
2016-10-25 17:58:18 +0530

    inline coding in jsx

--- voting_app/app/components/ProductList.js
+++ voting_app/app/components/ProductList.js
@@ -1,35 +1,35 @@
 var React = require('react');
 var Product = require('./Product');
 
-var AscSort = function({toggleSort}){
+var AscSort = function({onToggleSort}){
 	return (		
-		<a href="#" onClick={toggleSort}>
+		<a href="#" onClick={onToggleSort}>
         	<i className='large caret up icon'></i>
         </a>
 	);
 };
 
-var DescSort = function({toggleSort}){
+var DescSort = function({onToggleSort}){
 	return (		
-		<a href="#" onClick={toggleSort}>
+		<a href="#" onClick={onToggleSort}>
         	<i className='large caret down icon'></i>
         </a>
 	);
 };
 
 var ProductList = function({products, onUpVote, onDownVote, sortOrder, onToggleSort}){
-	var productsEl = products.map(function(product){
-		return (<Product product={product} key={product.id} onUpVote={onUpVote} onDownVote={onDownVote} />);
-	});
-
-	var sortEl = sortOrder === 0 ? <AscSort toggleSort={onToggleSort}/> : <DescSort toggleSort={onToggleSort}/>
-
 	return (
-		<div className='ui items'>
-			<div>
-				Sort: {sortEl}
+		<div>
+			<div className='ui items'>
+				<div>
+					Sort: { sortOrder === 0 ? <AscSort onToggleSort={onToggleSort}/> : <DescSort onToggleSort={onToggleSort}/> }
+				</div>
+				{
+					products.map(function(product){
+					return (<Product product={product} key={product.id} onUpVote={onUpVote} onDownVote={onDownVote} />);
+					})
+				}
 			</div>
-			{productsEl}
 		</div>
 	);
 };
